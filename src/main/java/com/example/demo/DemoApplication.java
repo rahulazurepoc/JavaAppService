dca7ee579799368c581ec79ac746c717214b0e8c
Rahul Marathe <rahulvmarathe@live.com>
2022-07-08 20:00:10 +0000

    integrate app configuration

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,17 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
 
 @SpringBootApplication
-@RestController
+@EnableConfigurationProperties(MessageProperties.class)
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@RequestMapping("/")
-	String sayHello() {
-		return "Hello World!";
-	}
 }
